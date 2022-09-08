f7997404a27cf7b09d06d15d4e6cfb84e622502f
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-09-08 11:53:56 +0530

    ROLL A DIE

--- game.java
+++ game.java
@@ -1,4 +1,5 @@
 package snakeLadder.com;
+import java.util.*;
 class player 
 {
 	int position= 0;
@@ -11,5 +12,8 @@ public class game
         System.out.println( "WELCOME TO SNAKE AND LADDER GAME" );
         player Player_1= new player();
  	    System.out.println("Player Position: "+Player_1.position);
+ 	    Random r=new Random();
+	    int result = r.nextInt(6);
+        System.out.println("You Rolled Die: "+result);
     }
 }
