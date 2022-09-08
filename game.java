bef6f71ed1f9d7a328499e36bcb413421800611b
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-09-08 11:48:27 +0530

    POSITION OF THE PLAYER

--- game.java
+++ game.java
@@ -1,9 +1,15 @@
 package snakeLadder.com;
-
+class player 
+{
+	int position= 0;
+}
 public class game 
 {
+	
     public static void main( String[] args )
     {
         System.out.println( "WELCOME TO SNAKE AND LADDER GAME" );
+        player Player_1= new player();
+ 	    System.out.println("Player Position: "+Player_1.position);
     }
 }
